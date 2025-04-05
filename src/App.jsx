dd8c7a3103fba9bd5cb08f42c3b2bb4a97361fe1
wilfrid <wilfrid.ndongmodjoumessi@fanuc.eu>
2025-04-05 14:51:00 +0200

    added text in my app file

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
     <>
      <h1>Hello world app</h1>
      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam officiis beatae ex esse fuga auten corporis minima hic assumenda fugiat ducimus, at possimus id a illum, quod incidunt voluptate illo?</p>
+      <h2>My first CI CD</h2>
+      <p>This is a test of my first CI/CD pipeline</p>
     </>
   )
 }
