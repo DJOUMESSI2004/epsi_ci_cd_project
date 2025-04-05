d38c7a3e0f9786236a7c28f32a2e6cb334c53c75
wilfrid <wilfrid.ndongmodjoumessi@fanuc.eu>
2025-04-05 14:42:39 +0200

    change repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/epsi_ci_cd_project/',
+  base: '/epsi_ci_cd_project/',
   plugins: [react()],
 })
